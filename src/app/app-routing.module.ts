c96785afccae4767fd4e1853d480c071182fada8
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2019-11-07 13:22:39 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'about', loadChildren: './about/about.module#AboutPageModule' },
+  { path: 'contact/:id', loadChildren: './contact/contact.module#ContactPageModule' },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  aboutPage = '/about'
+
+  constructor(private router: Router) {}
+
+  go(){
+     //should be an array as input
+    // this.router.navigate(["about"]);
+
+    //should be string as input
+    this.router.navigateByUrl("/about");
+  }
+
+  contact(){
+    //this.router.navigate(["contact/13"]);
+    this.router.navigate(["contact", 15]);
+  }
 
 }
